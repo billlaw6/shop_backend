90a7e9963ebb258a1b2fce54873f26652700d484
billlaw6 <liubin@gmail.com>
2018-07-24 16:49:52 +0800

    2018-07-24 Shop 1.0

--- shop_frontend/src/http/api.js
+++ shop_frontend/src/http/api.js
@@ -4,7 +4,7 @@ import store from '@/vuex-store/index'
 // import * as types from '@/vuex-store/types'
 import { router } from '@/router/index'
 
-axios.defaults.baseURL = 'http://123.56.115.20'
+// axios.defaults.baseURL = 'http://123.56.115.20'
 axios.defaults.timeout = 5000
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
 axios.defaults.headers.post['X-Requested-With'] = 'XMLHttpRequest'
